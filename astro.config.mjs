175cf0babb6f934cdad0cdffa6a2b6c97aef52fe
daniel-gallo <danielsantoangel@gmail.com>
2024-07-06 19:58:49 +0200

    Add math rendering in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import rehypeMathjax from "rehype-mathjax";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax]
+  }
 });
